ab0ac3b34317038fef237cbe2386521aa347aaa6
wwb0077 <1797373219@qq.com>
2017-07-20 22:37:18 +0800

    111

---
+++ studing/003-function.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwb
+ * Date: 2017/7/20
+ * Time: 22:20
+ */
+header("Content-Type:text/html; charset=utf-8");
+
+function jia($any){
+    $sum =0;
+    for($i=0;$i<=$any;$i++){
+        $sum += $i;
+    }
+    return $sum;
+}
+echo jia(100);
